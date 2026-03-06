585a450544783808469e4054e651aefa9ef1f7c0
SrPatoAC <alexsantiagorivastorres72@gmail.com>
2026-03-05 22:01:46 -0500

    Update server icons in servers-config.js

--- assets/servers-config.js
+++ assets/servers-config.js
@@ -1,7 +1,7 @@
 window.SERVERS_CONFIG = {
   // Logos PNG en /servers-icons o cualquier ruta válida del proyecto
   hostings: [
-    { name: 'SapphireHost', role: 'General Manager y CEO', icon: 'sapphire.png', fallback: 'SH' },
+    { name: 'SapphireHost', role: 'General Manager y CEO', icon: 'sapphire1.png', fallback: 'SH' },
     { name: 'EnderHost', role: 'Soporte Técnico y General', icon: 'ae6de9625dd7ca1978c1ec6805a922b5.png', fallback: 'EH' }
   ],
   servers: [
@@ -9,8 +9,8 @@ window.SERVERS_CONFIG = {
     { name: 'SumerLand', role: 'Staff Manager', icon: 'ae6de9625dd7ca1978c1ec6805a922b5.png', fallback: 'SL' },
     { name: 'MeteorMC', role: 'SS Manager - Head Admin', icon: 'ae6de9625dd7ca1978c1ec6805a922b5.png', fallback: 'MM' },
     { name: 'Prisma MC', role: 'SS Coordinator - Senior Mod', icon: 'ae6de9625dd7ca1978c1ec6805a922b5.png', fallback: 'PM' },
-    { name: 'KeniBox', role: 'SS Mentor - Mod+', icon: 'ae6de9625dd7ca1978c1ec6805a922b5.png', fallback: 'KB' },
-    { name: 'Atlantic MC', role: 'Head Admin', icon: 'ae6de9625dd7ca1978c1ec6805a922b5.png', fallback: 'AM' },
+    { name: 'KeniBox', role: 'SS Mentor - Mod+', icon: 'kenibox.png', fallback: 'KB' },
+    { name: 'Atlantic MC', role: 'Head Admin', icon: 'atlantismc.png', fallback: 'AM' },
     { name: 'LowBox', role: 'Admin', icon: 'ae6de9625dd7ca1978c1ec6805a922b5.png', fallback: 'LB' },
     { name: 'OverCraftNetwork', role: 'Admin', icon: 'ae6de9625dd7ca1978c1ec6805a922b5.png', fallback: 'OC' },
     { name: 'FlameCraft', role: 'Head Mod', icon: 'ae6de9625dd7ca1978c1ec6805a922b5.png', fallback: 'FC' },
